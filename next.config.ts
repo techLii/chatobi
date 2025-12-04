97845abe83a02249dc40d760726fc17cd695012e
techLii <eleeakim@gmail.com>
2025-12-04 13:16:06 +0300

    Fixed the CCORS error?

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    allowedDevOrigins: ["192.168.1.154", "chatobi.vercel.app"],
+  },
 };
 
 export default nextConfig;
